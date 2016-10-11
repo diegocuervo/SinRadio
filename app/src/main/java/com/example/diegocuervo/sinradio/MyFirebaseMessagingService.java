74717dd6b2373e953d7f1ec920031e3413bc5c28
Diego Cuervo <diegonoya93@hotmail.com>
2016-10-11 20:12:08 -0300

    ANDA FIREBASE

---
+++ app/src/main/java/com/example/diegocuervo/sinradio/MyFirebaseMessagingService.java
@@ -0,0 +1,32 @@
+package com.example.diegocuervo.sinradio;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Diego Cuervo on 10/10/16.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+
+    }
+
+
+}
